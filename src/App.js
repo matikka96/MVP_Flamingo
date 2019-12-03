c37c3324e9b264a1dbfb2ce79ae792237d87e9d5
matikka96 <31724958+matikka96@users.noreply.github.com>
2019-12-03 12:01:22 +0100

    Hello World + I/O demonstration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import logo from './logo.png';
-import './App.css';
+// import React from "react";
+// import logo from "./logo.png";
+// import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          "Hello world!" - Team Flamingo
-        </p>
-      </header>
-    </div>
-  );
+// function App() {
+//   state = {
+//     sliderValue: null
+//   }
+//   render() {
+//     return (
+//     <div className="App">
+//       <input type="range" min="1" max="100" value="50"></input>
+//     </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    rangeValue: 1
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.rangeValue}€</h1>
+        <input
+          style={{ zoom: "2" }}
+          type="range"
+          min="1"
+          max="100"
+          defaultValue={this.state.rangeValue}
+          onChange={e => this.setState({ rangeValue: e.target.value })}
+        ></input>
+        <div>
+          <button
+            onClick={() => alert(`${this.state.rangeValue}€ saved. Congratulation!`)}
+          >
+            Save money
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
